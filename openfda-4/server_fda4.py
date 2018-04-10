552683c415a87b1eccb32098924f8bfc2ee1c0b8
jaimecorton <corton35jaime@gmail.com>
2018-04-10 16:31:23 +0200

    working

--- openfda-4/server_fda4.py
+++ openfda-4/server_fda4.py
@@ -1,25 +1,51 @@
 import http.server
+import http.client
 import socketserver
 import json
 
-PORT = 8006
+socketserver.TCPServer.allow_reuse_address = True
 
-# HTTPRequestHandler class
+PORT = 8005
+
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    # GET
     def do_GET(self):
-#dhsagh
 
-
-
-
-
-        # Send response status code
         self.send_response(200)
 
-        # Send headers
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
+        if self.path == "/":
+            with open("search.html") as f:
+                message = f.read()
+                self.wfile.write(bytes(message, "utf8"))
+
+        elif "search" in self.path:
+            params = self.path.split("?")[1]
+            drug = params.split("&")[0].split("=")[1]
+
+            headers = {'User-Agent': 'http-client'}
+
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            conn.request("GET", '/drug/label.json?search=generic_name:' + drug, None, headers)
+            r1 = conn.getresponse()
+            print(r1.status, r1.reason)
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+
+            drugs = json.loads(repos_raw)
+            drugs = drugs['results']
+            drugs_id = "<ol>" + drugs[0]['id'] + "</ol>"
+            self.wfile.write(bytes(drugs_id, "utf8"))
+
+        return
+
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler = testHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
 
         # Send message back to client
         message = drugs_id
